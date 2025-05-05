b842ed9af01b6bc2075d2a67cb53be132b4bbdc2
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-05-05 22:35:29 +0600

    pilot

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('first/', views.first, name='first')
+    path('', views.first, name='first'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('minutes/', views.minutes, name='minutes')
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def first(request):
     return render(request, 'first.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def minutes(request):
+    return render(request, 'minutes.html')
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def signup(request):
+    return render(request, 'signup.html')
